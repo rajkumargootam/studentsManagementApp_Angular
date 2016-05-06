39e00045d7dd78a52a9c01f1eee6a33447145428
rajkumar <rajkumar.gootam@gmail.com>
2016-05-06 18:59:33 +0530

    added classes,section and student using rails api

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -16,12 +16,20 @@
       });
       vm.klassResp = resourceObj.get();
     }
-   
+
      vm.showSections = function(klassId){
+      vm.klassId = klassId;
       var resourceObj = $resource('http://pure-retreat-73401.herokuapp.com/api/v1/klasses/'+klassId+'/sections.json',{
         access_token:'TLVMLZCHEBSBAVTQJDV5LVTB7E8S74Q4'
       });
       vm.sectionResp = resourceObj.get();
+    }
+      vm.showsStudent = function(sectionId){
+       var resourceObj = $resource('http://pure-retreat-73401.herokuapp.com/api/v1/klasses/'+vm.klassId+'/sections/'+sectionId+'/students.json',{
+         access_token:'TLVMLZCHEBSBAVTQJDV5LVTB7E8S74Q4'
+       });
+       vm.studentResp = resourceObj.get();
+       console.log(studentResp);
      }
     init();
   }
